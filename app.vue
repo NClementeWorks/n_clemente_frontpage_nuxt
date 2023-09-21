9354aa2be32a2976b307fa868835327fbabf0c0e
Noliani Clemente Hernandez <44977963+nclemente@users.noreply.github.com>
2023-09-21 19:42:08 -0400

    initial comit

---
+++ app.vue
@@ -0,0 +1,13 @@
+
+
+<template>
+  <UContainer class="grid grid-cols-5">
+
+    <TheSideNav />
+
+    <NuxtPage
+      class="col-start-2 col-end-6 mx-4 px-4 "
+      />
+      
+  </UContainer>
+</template>
---
+++ components/TheSideNav.vue
@@ -0,0 +1,32 @@
+<!-- SideNav -->
+
+<script setup>
+  const links = [{
+    label: 'Profile',
+    avatar: {
+      src: 'https://avatars.githubusercontent.com/u/739984?v=4'
+    },
+    badge: 100
+  }, {
+    label: 'Installation',
+    icon: 'i-heroicons-home',
+    to: '#installation'
+  }, {
+    label: 'Vertical Navigation',
+    icon: 'i-heroicons-chart-bar',
+    to: '#vertical-navigation'
+  }, {
+    label: 'Command Palette',
+    icon: 'i-heroicons-command-line',
+    to: '#command-palette'
+  }]
+</script>
+
+<template>
+  <div class="flex fixed col-start-1 w-1/6 h-2/3 m-0">
+    <UVerticalNavigation
+      :links="links"
+      class="w-full my-auto"
+      />
+  </div>
+</template>
