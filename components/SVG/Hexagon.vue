87524c7520ef0acc33c6b159204f850047a09548
Noliani Clemente Hernandez <44977963+nclemente@users.noreply.github.com>
2023-09-25 15:36:08 -0400

    move SVG files into components

---
+++ components/SVG/Hexagon.vue
@@ -0,0 +1,41 @@
+<template>
+  
+  <svg class="hexagon" width="109" height="122" viewBox="0 0 109 122" fill="none" xmlns="http://www.w3.org/2000/svg">
+    
+    <defs>
+      <!-- <filter id="inset-shadow">
+        <feOffset dx="-4" dy="4" result="offsetblur" />
+        <feGaussianBlur stdDeviation="5" />
+        <feComposite operator="in" in2="SourceAlpha" />
+      </filter> -->
+          
+      <filter id="inset-shadow" x="-50%" y="-50%" width="200%" height="200%">
+        <feComponentTransfer in=SourceAlpha>
+          <feFuncA type="table" tableValues="1 0" />
+        </feComponentTransfer>
+        <feGaussianBlur stdDeviation="4"/>
+        <feOffset dx="-4" dy="6" result="offsetblur"/>
+        <feFlood flood-color="rgb(0, 0, 0)" result="color"/>
+        <feComposite in2="offsetblur" operator="in"/>
+        <feComposite in2="SourceAlpha" operator="in" />
+        <feMerge>
+          <feMergeNode in="SourceGraphic" />
+          <feMergeNode />
+        </feMerge>
+      </filter>
+    </defs>
+
+    <path
+      class="hexagon_path"
+      d="M3.8147e-06 85.84C3.8147e-06 89.8822 2.1669 93.6153 5.67106 95.6425L48.8351 120.489C52.3393 122.504 56.6482 122.504 60.1524 120.489L103.329 95.6425C106.833 93.6276 109 89.8822 109 85.84V36.1722C109 32.13 106.833 28.397 103.329 26.3697L60.1648 1.51117C56.6606 -0.503724 52.3517 -0.503724 48.8475 1.51117L5.68346 26.3573C2.1793 28.3722 0.0123843 32.1176 0.0123843 36.1598V85.84H3.8147e-06Z"
+      fill="#1DBBE2"
+      filter="url(#inset-shadow)"
+      />
+  </svg>
+
+</template>
+
+<style lang="sass">
+.hexagon
+  height: fit-content
+</style>
---
+++ components/SVG/TextBubble.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- Generator: Adobe Illustrator 26.2.1, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+  <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="40px" y="0px"
+    viewBox="0 0 285 207" style="enable-background:new 0 0 274 207;" xml:space="preserve">
+
+    <g>
+      <g>
+        <path class="st0" d="M137,0C61.3,0,0,39.2,0,87.5c0,40.9,43.9,75.2,103.3,84.8c7.3,9.9,5.4,23.8-3.7,34.7
+          c13.6-4.7,29.1-16,34.1-32c1.1,0,2.2,0,3.3,0c75.7,0,137-39.2,137-87.5C274,39.2,212.7,0,137,0z"
+          transform="translate(10,0)" />
+      </g>
+    </g>
+    
+  </svg>
+</template>
+
+<style type="text/css">
+.st0 {
+  fill:#FFFFFF;
+  filter: drop-shadow(-4px 4px 4px rgb(0 0 0 / 0.25));
+}
+</style>
