18667080622f80bbdfab97807a9ec63762ba18bd
Noliani Clemente Hernandez <44977963+nclemente@users.noreply.github.com>
2024-05-01 14:01:47 -0400

    get hexagon svg path from constant value

--- components/SVG/Hexagon.vue
+++ components/SVG/Hexagon.vue
@@ -1,3 +1,11 @@
+<!-- HexagonSingle -->
+
+<script setup>
+
+  const hexagon = useHexagons ()
+
+</script>
+
 <template>
   
   <svg class="hexagon" width="109" height="122" viewBox="0 0 109 122" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -27,9 +35,9 @@
 
     <path
       class="hexagon_path"
-      d="M3.8147e-06 85.84C3.8147e-06 89.8822 2.1669 93.6153 5.67106 95.6425L48.8351 120.489C52.3393 122.504 56.6482 122.504 60.1524 120.489L103.329 95.6425C106.833 93.6276 109 89.8822 109 85.84V36.1722C109 32.13 106.833 28.397 103.329 26.3697L60.1648 1.51117C56.6606 -0.503724 52.3517 -0.503724 48.8475 1.51117L5.68346 26.3573C2.1793 28.3722 0.0123843 32.1176 0.0123843 36.1598V85.84H3.8147e-06Z"
       fill="#1DBBE2"
       filter="url(#inset-shadow)"
+      :d="hexagon.path"
       />
   </svg>
 
--- composables/hexagons.ts
+++ composables/hexagons.ts
@@ -1,4 +1,6 @@
 export const useHexagons = () => {
+
+  const path = 'M3.8147e-06 85.84C3.8147e-06 89.8822 2.1669 93.6153 5.67106 95.6425L48.8351 120.489C52.3393 122.504 56.6482 122.504 60.1524 120.489L103.329 95.6425C106.833 93.6276 109 89.8822 109 85.84V36.1722C109 32.13 106.833 28.397 103.329 26.3697L60.1648 1.51117C56.6606 -0.503724 52.3517 -0.503724 48.8475 1.51117L5.68346 26.3573C2.1793 28.3722 0.0123843 32.1176 0.0123843 36.1598V85.84H3.8147e-06Z'
   
   const hexagon_width_px = 109
   const hexagon_height_px = 122
@@ -33,6 +35,7 @@ export const useHexagons = () => {
 
   return {
 
+    path,
     get default_width_px () { return hexagon_width_px },
     get default_height_px () { return hexagon_height_px },
 
