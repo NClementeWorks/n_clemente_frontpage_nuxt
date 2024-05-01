910ffbf3962caebe521a2d441aab0fa92ea35f77
Noliani Clemente Hernandez <44977963+nclemente@users.noreply.github.com>
2024-05-01 16:57:10 -0400

    reverse animation order for Hero hexagons

--- composables/animations/hexagons/hero.ts
+++ composables/animations/hexagons/hero.ts
@@ -7,8 +7,8 @@ export const useAnimationsHexagonsHero = () => {
 
   const tl_hero = gsap.timeline ({})
 
-  const hero_cols = [ 0, 1, 0, 1, 3, 2, 1 ]
-  const hero_rows = [ 0, 1, 2, 3, 3, 4, 5 ]
+  const hero_cols = [ 0, 1, 0, 1, 3, 2, 1 ].reverse ()
+  const hero_rows = [ 0, 1, 2, 3, 3, 4, 5 ].reverse ()
   
   function init_start ( screen_width : Ref<number> ) : any {
     return {
