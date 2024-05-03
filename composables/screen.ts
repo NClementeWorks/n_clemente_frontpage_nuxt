a2cd5a49765b32c0e5c4cb072a2fca398595588b
Noliani Clemente Hernandez <44977963+nclemente@users.noreply.github.com>
2024-05-03 16:06:48 -0400

    add function on screen.ts to get dom element reactive position and dimensions

--- composables/screen.ts
+++ composables/screen.ts
@@ -1,3 +1,5 @@
+import { useElementBounding, useWindowScroll } from '@vueuse/core'
+
 export const useScreen = () => {
 
   function get_top ( el : HTMLElement | null ) : any {
@@ -10,6 +12,57 @@ export const useScreen = () => {
     return el?.offsetParent
       ? el?.offsetLeft + get_left ( el?.offsetParent as HTMLElement )
       : el?.offsetLeft
+  }
+  
+  /**
+   * 
+   * @param el 
+   * @returns 
+   */
+  function get_element_screen_props ( el : HTMLElement ) : ElementScreenProperties {
+
+    //
+    // VueUse element bounding box
+    //
+    const {
+      top,
+      left,
+      width,
+      height,
+    } = useElementBounding ( el )
+    
+    //
+    // VueUse window scroll position
+    //
+    const {
+      x: scroll_x,
+      y: scroll_y,
+    } = useWindowScroll ()
+
+    //
+    //  calculate absolute x position
+    //
+    const x = ref ( 0 )
+    on_screen_ready ( () => {
+      x.value = top.value + scroll_x.value 
+    })
+
+    //
+    //  calculate absolute y position
+    //
+    const y = ref ( 0 )
+    on_screen_ready ( () => {
+      y.value = top.value + scroll_y.value 
+    })
+
+    return {
+      x,
+      y,
+      top,
+      left,
+      width,
+      height,
+    }
   }
   
   // const draw_mark = (
@@ -33,6 +86,7 @@ export const useScreen = () => {
 
   return {
     get_top,
-    get_left,   
+    get_left,
+    get_element_screen_props,
   }
 }
---
+++ types/index.ts
@@ -0,0 +1,14 @@
+export { };
+
+declare global {
+
+  type ElementScreenProperties = {
+    x : Ref<number>,
+    y : Ref<number>,
+    top : Ref<number>,
+    left : Ref<number>,
+    width : Ref<number>,
+    height : Ref<number>,
+  }
+
+}
