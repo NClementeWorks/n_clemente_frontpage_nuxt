09905e2537a873b75f3b704ad3c83bb1f333986d
Noliani Clemente Hernandez <44977963+nclemente@users.noreply.github.com>
2024-05-03 16:12:34 -0400

    add template store to handle global template refs

---
+++ stores/template.ts
@@ -0,0 +1,47 @@
+export const useTemplateStore = defineStore ( 'template', () => {
+
+  const screen = useScreen ()
+
+  //
+  // store html/ref elements
+  //
+  const template_elements = ref ( {} )
+
+  /**
+   * Add element to template refs map and create screen properties references
+   * @param key map key
+   * @param el HTML element
+   */
+  function add_element ( key : string, el : HTMLElement ) : void {
+    template_elements.value [ key ] = {
+      el,
+      props: screen.get_element_screen_props ( el )
+    }
+  }
+
+  /**
+   * Retrieve element with screen properties information
+   * @param key map key
+   * @returns object containing the HTML element and the screen properties
+   */
+  function get_element ( key : string ) : {
+    el: HTMLElement,
+    props: ElementScreenProperties,
+  } {
+    return template_elements.value [ key ]
+  }
+
+  /**
+   * Clear out elemnt and properties from the template refs map
+   * @param key map key
+   */
+  function clear_element ( key : string ) : void {
+    template_elements.value [ key ] = null
+  }
+
+  return {
+    add_element,
+    get_element,
+    clear_element,
+  }
+})
