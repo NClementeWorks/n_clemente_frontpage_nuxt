18345002d3772ac563348155721722300311343b
Noliani Clemente Hernandez <44977963+nclemente@users.noreply.github.com>
2024-05-20 08:58:11 -0400

    store/template.ts add elements_ready variable to monitor when all elements in home page are rendered
    This is needed with commit da465c58c67c09b43748c5218edae39e001318ed

--- stores/template.ts
+++ stores/template.ts
@@ -1,4 +1,10 @@
+import { Value } from 'sass'
 import { watch } from 'vue'
+
+export interface TemplateElement {
+  el: Ref<HTMLElement>,
+  props: ElementScreenProperties,
+}
 
 export const useTemplateStore = defineStore ( 'template', () => {
 
@@ -7,26 +13,40 @@ export const useTemplateStore = defineStore ( 'template', () => {
   //
   // store html/ref elements
   //
-  const template_elements = ref ( {} )
+  const template_elements = ref<any> ( {} )
+  const home_elements = ref<any> ( {
+    stack_items: null,
+    stack_first_icon: null,
+    cta_section: null,
+    use_case_imgs: null,
+    top_skills_flower: null,
+    top_skills_first_icon: null,
+  } )
 
   /**
    * Add element to template refs map and create screen properties references
    * @param key map key
    * @param el HTML element
    */
-  function add_element ( key : string, el : HTMLElement ) : void {
+  function add_element ( key : string, el : Ref<HTMLElement | null> ) : void {
     template_elements.value [ key ] = {
       el,
       props: screen.get_element_screen_props ( el )
     }
+    
+    if ( home_elements.value [ key ] !== undefined )
+      home_elements.value [ key ] = el
   }
 
-  function add_elements ( key : string, el_arr : Ref<Array<HTMLElement>> ) : void {
+  function add_elements ( key : string, el_arr : Ref<HTMLElement[]> ) : void {
     watch ( () => el_arr?.value?.length, () => {
       template_elements.value [ key ] = el_arr?.value.map ( ( el : HTMLElement ) => ({
         el,
         props: screen.get_element_screen_props ( el )
       }))
+      
+      if ( home_elements.value [ key ] !== undefined )
+        home_elements.value [ key ] = el_arr
     })
   }
 
@@ -35,10 +55,7 @@ export const useTemplateStore = defineStore ( 'template', () => {
    * @param key map key
    * @returns object containing the HTML element and the screen properties
    */
-  function get_element ( key : string ) : {
-    el: HTMLElement,
-    props: ElementScreenProperties,
-  } {
+  function get_element ( key : string ) : TemplateElement | TemplateElement[] {
     return template_elements.value [ key ]
   }
 
@@ -50,8 +67,17 @@ export const useTemplateStore = defineStore ( 'template', () => {
     template_elements.value [ key ] = null
   }
 
-  const hexagon_paths = ref ([])
-  const cta_hexagon_paths = ref ([])
+  const hexagon_paths = ref<SVGElement[]> ([])
+  const cta_hexagon_paths = ref<SVGElement[]> ([])
+
+  const elements_ready = ref ( false )
+
+  watch ( home_elements, 
+    () => {
+      elements_ready.value = Object.entries(home_elements.value).every ( ([ key, element ] : [ string, any ]) => !!element?.length || !!element)
+    }, {
+    deep: true,
+  })
 
   return {
     add_element,
@@ -60,5 +86,9 @@ export const useTemplateStore = defineStore ( 'template', () => {
     clear_element,
     hexagon_paths,
     cta_hexagon_paths,
+
+    template_elements,
+
+    elements_ready,
   }
 })
