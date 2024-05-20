d5906081a9c8f732d6d9b0f4b862661d82b442f6
Noliani Clemente Hernandez <44977963+nclemente@users.noreply.github.com>
2024-05-20 13:32:53 -0400

    Update TheCTA.vue
    TheCTA.vue remove unused code

--- components/TheCTA.vue
+++ components/TheCTA.vue
@@ -8,8 +8,6 @@
   const hexagons = useHexagons ()
   const menu = useMenu ()
 
-  const background_hexagons_amount = computed ( () => Math.ceil ( display.width.value / hexagons.default_width_px ) + 1 )
-
   const cta_el = ref ()
   template.add_element ( 'cta_section', cta_el )
   
@@ -17,25 +15,6 @@
 
 <template>
   <div class="cta_section" ref="cta_el">
-
-    <!-- <div
-      v-for="line in 2" :key="line"
-      :style="{
-        position: 'absolute',
-        top: `${ line % 2 ? '-' : '' }${ hexagons.default_height_px / 2.75 }px`,
-        left: line % 2 ? '0' : `-${ hexagons.default_width_px / 2 }px`,
-        maxWidth: '100%',
-      }"
-      >
-      <SVGHexagon
-        v-for="hex in background_hexagons_amount"
-        :key="hex"
-        :style="{
-          position: 'absolute',
-          left: ( hex - 1 ) * hexagons.default_width_px,
-        }"  
-        />
-    </div> -->
 
     <PrimaryBtn
       :to="menu.nav.contact_form.link"
